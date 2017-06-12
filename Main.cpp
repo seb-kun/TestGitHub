34446b1132ffa0c11c3cbc2b78928ca6684ef035
seb-kun <tixier.seb@gmail.com>
2017-06-12 14:15:46 +0800

    Added Android platform in master branch

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,16 @@
 #include Example.h
 
 #define NO_ERROR (0)
+#define ANDROID_PLATFORM (1)
 
 int main ()
 {
 	int returnValue = NO_ERROR;
+	
+	if( int os == 3 )
+	{
+		return ANDROID_PLATFORM;
+	}
 	
 	return NO_ERROR;
 }
