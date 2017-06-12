c2ccd2bd4adcd11e7a9d05f57ecc6f35aab7f0a3
seb-kun <tixier.seb@gmail.com>
2017-06-12 14:14:31 +0800

    Modified main.cpp with win platform

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,16 @@
 #include Example.h
 
 #define NO_ERROR (0)
+#define WIN_PLATFORM (1)
 
 int main ()
 {
 	int returnValue = NO_ERROR;
+	
+	if ( int platform == 2 )
+	{
+		return WIN_PLATFORM;
+	}
 	
 	return NO_ERROR;
 }
