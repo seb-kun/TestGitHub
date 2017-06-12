80c3f93e6b5bb668ed7a279ae3b923de11ae8fcd
seb-kun <tixier.seb@gmail.com>
2017-06-12 14:20:13 +0800

    Added android example

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 
 #define NO_ERROR (0)
 #define ANDROID_PLATFORM (1)
+#define IOS_PLATFORM (2)
 
 int main ()
 {
@@ -10,6 +11,11 @@ int main ()
 	if( int os == 3 )
 	{
 		return ANDROID_PLATFORM;
+	}
+	
+	if( int os == 2 )
+	{
+		return IOS_PLATFORM;
 	}
 	
 	return NO_ERROR;
